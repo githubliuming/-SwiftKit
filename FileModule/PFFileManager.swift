20f18207f2651d247f75271d9b2f0e6e9d5658ef
liuming <1969489778@qq.com>
2020-07-05 15:40:49 +0800

    格式化代码

--- FileModule/PFFileManager.swift
+++ FileModule/PFFileManager.swift
@@ -7,32 +7,30 @@
 //
 
 import Foundation
-extension FileManager
-{
+extension FileManager {
     /// 沙盒路径
-    public func documentDirectorPath()-> String
-    {
-        return  NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask,true).last!
+    public func documentDirectorPath() -> String {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
     }
+
     /// cathe 文件路径
-    public func catcheDirectorPath () -> String
-    {
-        return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last!;
+    public func catcheDirectorPath() -> String {
+        return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).last!
     }
+
     /// library 文件路径
-    public func libraryDirectorPath () -> String
-    {
+    public func libraryDirectorPath() -> String {
         return NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).last!
     }
+
     /// 沙盒根路径
-    public func sanboxHomeDirectorPath() -> String
-    {
-        return NSHomeDirectory();
+    public func sanboxHomeDirectorPath() -> String {
+        return NSHomeDirectory()
     }
-    
-    //验证文件夹是否存在
-    public func directoryIsExists (path: String) -> Bool {
-        var directoryExists = ObjCBool.init(false)
+
+    // 验证文件夹是否存在
+    public func directoryIsExists(path: String) -> Bool {
+        var directoryExists = ObjCBool(false)
         let fileExists = FileManager.default.fileExists(atPath: path, isDirectory: &directoryExists)
         return fileExists && directoryExists.boolValue
     }
--- StoreboardModule/PFStoreboard.swift
+++ StoreboardModule/PFStoreboard.swift
@@ -8,26 +8,21 @@
 
 import Foundation
 import UIKit
-open class PFStoreboard<T:UIViewController>
-{
-    
+open class PFStoreboard<T: UIViewController> {
     /// 加在Main Storeboard 里面的viewcontroller
     /// - Parameter storeboardID: storeboardID
     /// - Returns: UIViewController
-    open class func loadVCFromeMainStoreBoard(storeboardID:String) -> T
-    {
+    open class func loadVCFromeMainStoreBoard(storeboardID: String) -> T {
         return loadVCFromeStoreboard(storeboarName: "Main", storeboardID: storeboardID)
     }
-    
+
     /// 加在 制定storeboard 上的viewcontroller
     /// - Parameters:
     ///   - storeboarName: 指定storeboard
     ///   - storeboardID: storeboardID
     /// - Returns: UIViewController
-    open class func loadVCFromeStoreboard(storeboarName:String,storeboardID:String ) ->T
-    {
-        let orderTVC = UIStoryboard.init(name: storeboarName, bundle: nil).instantiateViewController(withIdentifier: storeboardID)
+    open class func loadVCFromeStoreboard(storeboarName: String, storeboardID: String) -> T {
+        let orderTVC = UIStoryboard(name: storeboarName, bundle: nil).instantiateViewController(withIdentifier: storeboardID)
         return orderTVC as! T
     }
-    
 }
