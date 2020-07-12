c0ce6dfd78d632a659b2d0f6bad69c72cb419356
liuming <1969489778@qq.com>
2020-07-12 14:23:31 +0800

    收集轮播图

---
+++ UIKit/LYBaseNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  LYBaseNavigationController.swift
+//  WKCat
+//
+//  Created by anita on 2020/7/12.
+//  Copyright © 2020 anita. All rights reserved.
+//
+
+import UIKit
+
+class LYBaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
